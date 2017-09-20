0a1d6a7fbd35502519a17e25400a51b3e0c6b9b5
金飞 <28206254@qq.com>
2017-09-20 18:57:31 +0800

    s

--- src/app/pages/examples/settings/settings.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './settings.component.html',
-})
-export class SettingsComponent {
-    lf: string = `{`;
-    rf: string = `}`;
-}
--- src/app/pages/index.service.ts
+++ src/app/pages/index.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
-import { data } from '../shared/data/Data1';
 
 @Injectable()
 export class IndexService {
-    getData() {
-        return data;
-    }
+    
 }
